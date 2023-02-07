e1d52db0c19499c8acf5e81187b36ce162a7014a
hyeonsik <dgt4862@gmail.com>
2023-02-07 19:45:59 +0900

    husky, eslint, prettier setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,23 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+    env: {
+    es6: true,
+        node: true,
+        jest: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+    plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
@@ -13,4 +28,19 @@ module.exports = {
       },
     },
   ],
+    rules: {
+        indent: ['error', 2, { SwitchCase: 1 }],
+        quotes: ['error', 'single', { avoidEscape: true }],
+
+        'no-empty-function': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        'react/display-name': 'off',
+        'react/prop-types': 'off',
+        'prettier/prettier': 'error',
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
 };
