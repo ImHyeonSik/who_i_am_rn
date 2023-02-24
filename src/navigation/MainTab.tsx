01090f1636872374987e5bbd645b1076ee54adc1
hyeonsik <dgt4862@gmail.com>
2023-02-24 13:23:01 +0900

    CHORE SwitchSelector clone

--- src/navigation/MainTab.tsx
+++ src/navigation/MainTab.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 import Dashboard from '../view/Dsahboard';
 import SecondView from '../view/SecondView';
+import ThirdView from '../view/ThirdView';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,6 +12,7 @@ const MainTab = () => {
     <Tab.Navigator initialRouteName={'Dashboard'}>
       <Tab.Screen component={Dashboard} name={'Dashboard'} />
       <Tab.Screen component={SecondView} name={'SecondView'} />
+      <Tab.Screen component={ThirdView} name={'ThirdView'} />
     </Tab.Navigator>
   );
 };
